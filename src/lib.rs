05fe1ab5a85011480c330e85c22e06dabce57ee0
T4toun3 <oettam32@gmail.com>
2021-01-18 13:01:59 +0100

    replace .split("\n") by .lines()

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ impl Paranagram {
 
         // Parse the content of the data file to create an vec of all word
         let mut sacamot: Vec<String> = buffer
-            .split("\n")
+            .lines()
             .filter_map(|s| {
                 if s.len() > max_len {
                     max_len = s.len()
